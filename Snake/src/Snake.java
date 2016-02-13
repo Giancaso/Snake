2417f153ab0ac79414ba15ddb63e2e080914f1b4
ADAMIALESSANDRO <ADAMIALESSANDRO@Lab06_06.itseinaudi.local>
2016-02-13 11:40:21 +0100

    aggiunto canvas e i button

--- Snake/src/Snake.java
+++ Snake/src/Snake.java
@@ -4,10 +4,15 @@ import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Canvas;
+import org.eclipse.wb.swt.SWTResourceManager;
 
 public class Snake {
 
-	protected Shell shell;
+	protected Shell shlSnake;
+	
+	private final int Cx = 500;
+	private final int Cy = 400;
 
 	/**
 	 * Launch the application.
@@ -28,9 +33,9 @@ public class Snake {
 	public void open() {
 		Display display = Display.getDefault();
 		createContents();
-		shell.open();
-		shell.layout();
-		while (!shell.isDisposed()) {
+		shlSnake.open();
+		shlSnake.layout();
+		while (!shlSnake.isDisposed()) {
 			if (!display.readAndDispatch()) {
 				display.sleep();
 			}
@@ -41,9 +46,33 @@ public class Snake {
 	 * Create contents of the window.
 	 */
 	protected void createContents() {
-		shell = new Shell();
-		shell.setSize(450, 300);
-		shell.setText("SWT Application");
+		shlSnake = new Shell();
+		shlSnake.setSize(735, 459);
+		shlSnake.setText("Snake");
+		
+		Canvas canvas = new Canvas(shlSnake, SWT.BORDER);
+		canvas.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+		canvas.setBounds(10, 10, 500, 400);
+		
+		Button btnStart = new Button(shlSnake, SWT.NONE);
+		btnStart.setBounds(584, 10, 75, 25);
+		btnStart.setText("START");
+		
+		Button btnSu = new Button(shlSnake, SWT.NONE);
+		btnSu.setBounds(584, 239, 60, 60);
+		btnSu.setText("^");
+		
+		Button btnSx = new Button(shlSnake, SWT.NONE);
+		btnSx.setBounds(518, 305, 60, 60);
+		btnSx.setText("<");
+		
+		Button btnGiu = new Button(shlSnake, SWT.NONE);
+		btnGiu.setBounds(584, 305, 60, 60);
+		btnGiu.setText("v");
+		
+		Button btnDx = new Button(shlSnake, SWT.NONE);
+		btnDx.setBounds(649, 305, 60, 60);
+		btnDx.setText(">");
 
 	}
 }

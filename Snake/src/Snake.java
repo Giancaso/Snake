a701a177c0be8268013d81e6b4fe1b4aa5e77a88
ADAMIALESSANDRO <ADAMIALESSANDRO@Lab06_06.itseinaudi.local>
2016-02-13 11:54:11 +0100

    aggiunto i tasti

--- Snake/src/Snake.java
+++ Snake/src/Snake.java
@@ -53,5 +53,25 @@ public class Snake {
 		Canvas canvas = new Canvas(shlSnake, SWT.BORDER);
 		canvas.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
 		canvas.setBounds(10, 10, 500, 400);
+		
+		Button btnStart = new Button(shlSnake, SWT.NONE);
+		btnStart.setBounds(582, 10, 60, 25);
+		btnStart.setText("START");
+		
+		Button btnSu = new Button(shlSnake, SWT.NONE);
+		btnSu.setBounds(582, 285, 60, 60);
+		btnSu.setText("^");
+		
+		Button btnSx = new Button(shlSnake, SWT.NONE);
+		btnSx.setText("<");
+		btnSx.setBounds(516, 351, 60, 60);
+		
+		Button btnGiu = new Button(shlSnake, SWT.NONE);
+		btnGiu.setText("v");
+		btnGiu.setBounds(582, 351, 60, 60);
+		
+		Button btnDx = new Button(shlSnake, SWT.NONE);
+		btnDx.setText(">");
+		btnDx.setBounds(649, 350, 60, 60);
 	}
 }

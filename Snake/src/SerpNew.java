544abc1be6dccf8861e1abf416f578d6557fe2de
bressanelia <bressanelia@Lab06_07.itseinaudi.local>
2016-02-23 12:25:09 +0100

    classe Serp

---
+++ Snake/src/SerpNew.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class SerpNew {
+	
+	private final int NUM_MAX_CUBI = 50;
+	private ArrayList<Cubo> elenco;
+	
+	public SerpNew(){
+		elenco = new ArrayList<Cubo> (NUM_MAX_CUBI);
+	}
+	
+	public void muovi(String m) {
+		switch (m) {
+		case "dx":
+			for(int i=0; i<elenco.size(); i++){
+				elenco.get(i).setX(elenco.get(i).getX()+elenco.get(i).getL());
+			}
+			break;
+			
+		case "sx":
+			for(int i=0; i<elenco.size(); i++){
+				elenco.get(i).setX(elenco.get(i).getX()-elenco.get(i).getL());
+			}
+			break;
+		}
+	}
+}
